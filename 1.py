1a6e27a399df2ff0559648bc3a0fd34a393251e6
Filip Mocanca <fmocanca@rcube.lu>
2025-04-14 17:23:20 +0200

    first commit

---
+++ 1.py
@@ -0,0 +1,15 @@
+age=7+5
+'''
+this is my bla
+bla lalalal
+'''
+name="Krish"
+
+if age>30:
+    print(age)
+else:
+    Name=""
+    Name=input("What's my name: ")
+    print(Name+" has "+str(age)+" years old")
+
+
---
+++ ageof.py
@@ -0,0 +1,4 @@
+age_isa=int(input("How old is Isabelle:"))
+age_papa=age_isa+35
+age_mama=age_isa+33
+print("Isabelle is "+str(age_isa)+" years old and her father is "+str(age_papa)+" years old and her mother is "+str(age_mama)+" years old")
